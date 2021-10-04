1fc9fcd42d7f26d6cb763795d583cde01de6413e
nicholas-long <nicholas.eden.long@gmail.com>
2021-10-03 19:36:24 -0500

    tor dealing with timeouts

--- frpc-tor/run-frpc.sh
+++ frpc-tor/run-frpc.sh
@@ -10,7 +10,7 @@ else
 fi
 
 /opt/gen-ini.sh > /opt/frpc.ini
-tor&
+tor --controlport 9051&
 
 echo socat TCP4-LISTEN:8000,fork SOCKS4A:localhost:$SERVER_IP:$SERVER_PORT,socksport=9050
 socat TCP4-LISTEN:8000,fork SOCKS4A:localhost:$SERVER_IP:$SERVER_PORT,socksport=9050&
@@ -20,6 +20,14 @@ sleep 10
 for x in $(seq 1 10)
 do
     echo running frpc
-    /opt/frp/bin/frpc
+    rm frpc-output.log
+    /opt/frp/bin/frpc | tee frpc-output.log
+    grep 'start proxy success' frpc-output.log
+    if [ $? -eq 0 ]
+    then
+        echo found one!
+    else
+        python3 reset-tor.py
+    fi
 done
 
