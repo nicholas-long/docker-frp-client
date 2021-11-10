47059dad661f4de2b659138bdfb56fee34fa48b8
nicholas-long <nicholas.eden.long@gmail.com>
2021-11-10 15:21:35 -0600

    test

--- worker/entry.sh
+++ worker/entry.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-tor --controlport 9051&
-sleep 10
+# tor --controlport 9051&
+# sleep 10
 
 amqp-consume -u amqp://rabbit -q "test" -e "amq.topic" -r "worker1" /opt/worker/script.sh
--- worker/script.sh
+++ worker/script.sh
@@ -2,14 +2,15 @@
 
 read args
 echo processing $args
-export SERVER_IP=$(echo "$args" | cut -d ' ' -f 1)
-export SERVER_PORT=$(echo "$args" | cut -d ' ' -f 2)
-export REMOTE_PORT=8999
-export LOCAL_PORT=80
-# timeout 55 /opt/run-frpc.sh $args 8999 80 > output
-timeout 55 /opt/exec-target.sh > output
+# export SERVER_IP=$(echo "$args" | cut -d ' ' -f 1)
+# export SERVER_PORT=$(echo "$args" | cut -d ' ' -f 2)
+# export REMOTE_PORT=8999
+# export LOCAL_PORT=80
+rm output 2>/dev/null
+timeout 15 /opt/run-frpc.sh $args 8999 80 | tee output
+# timeout 55 /opt/exec-target.sh > output
 
-cat output
+# cat output
 grep 'start proxy success' output
 if [ $? -eq 0 ]
 then
@@ -19,10 +20,10 @@ then
     mysql -h mysql -u mysql -pmysql -D frpsscan -e "insert into targets (ip, port, valid) values ('$host', $port, 1);"
     python3 /opt/worker/push.py
 fi
-grep 'dial tcp 127.0.0.1:8000: connect: connection refused' output
-if [ $? -eq 0 ]
-then
-    ps -ef | grep tor | grep -v grep | awk '{print $2}' | xargs kill
-    ps -ef | grep amqp-consume | grep -v grep | awk '{print $2}' | xargs kill
-    exit 1
-fi
+# grep 'dial tcp 127.0.0.1:8000: connect: connection refused' output
+# if [ $? -eq 0 ]
+# then
+#     ps -ef | grep tor | grep -v grep | awk '{print $2}' | xargs kill
+#     ps -ef | grep amqp-consume | grep -v grep | awk '{print $2}' | xargs kill
+#     exit 1
+# fi
